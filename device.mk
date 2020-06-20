c54eebf806b662d8a41c349f137c4f1dfd32ec0b
wenchangliu <wenchangliu@google.com>
2020-06-20 18:47:09 +0800

    munch: video: enbale adaptive b-frame capability
    'enc_default_bframe_count' set to 1.
    'enc_max_mbs_per_sec' set to max-hw-load.
    
    Bug: 156724722
    Test: run ECO experiments
    Change-Id: I884be887e9ae71bb9f8778f8bd0f5dd728d1ba7e

--- device.mk
+++ device.mk
@@ -270,7 +270,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_kona.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/configs/media/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json
 
 # NFC
 PRODUCT_PACKAGES += \
