ec3c4d134d71ed3deb79f70e3ca20e4b98802bcf
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-28 01:42:19 +0100

    munch: Move to the new libinit implementation
    Change-Id: I4207a94ff62f0c515c91e186f0ace348c6db7b1d

--- libinit/libvariant_xiaomi_munch.cpp
+++ libinit/libvariant_xiaomi_munch.cpp
@@ -1,15 +1,12 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <libinit_dalvik_heap.h>
-#include <libinit_variant.h>
+#include <libvariant.h>
 
-#include "vendor_init.h"
-
-static const variant_info_t munch_in_info = {
+static const variant_info munch_in_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
@@ -22,7 +19,7 @@ static const variant_info_t munch_in_info = {
     .nfc = true,
 };
 
-static const variant_info_t munch_global_info = {
+static const variant_info munch_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -35,7 +32,7 @@ static const variant_info_t munch_global_info = {
     .nfc = true,
 };
 
-static const variant_info_t munch_info = {
+static const variant_info munch_info = {
     .hwc_value = "",
     .sku_value = "",
 
@@ -48,13 +45,8 @@ static const variant_info_t munch_info = {
     .nfc = true,
 };
 
-static const std::vector<variant_info_t> variants = {
+const std::vector<variant_info> variants = {
     munch_in_info,
     munch_global_info,
     munch_info,
 };
-
-void vendor_load_properties() {
-    search_variant(variants);
-    set_dalvik_heap();
-}
