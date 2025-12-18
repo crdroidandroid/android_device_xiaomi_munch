aa07e6a77f3b03dd1cae9e608feeac3e64a5b449
SenseiiX <senseixdev@gmail.com>
2025-12-18 19:18:44 +0800

    munch: Use global fp for BuildFingerprint override
    Change-Id: I5d9c81dd9dc059be75c7b352fb7db126a09edd87
    Signed-off-by: SenseiiX <senseixdev@gmail.com>
    Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.12.0.ULMMIXM release-keys" \
-    BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.12.0.ULMMIXM:user/release-keys
+    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.12.0.ULMMIXM:user/release-keys
